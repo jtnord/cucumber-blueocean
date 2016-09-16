cc2581b96a0ba573f14ae4df5c74e8757a253d3a
James Nord <jnord+github@cloudbees.com>
2016-09-16 15:14:29 -0700

    initial project structure

---
+++ src/test/js/Sample-spec.js
@@ -0,0 +1,11 @@
+/**
+ * Sample js test file
+ */
+import { assert } from 'chai';
+
+describe('sampleTest', () => {
+
+  it('rassert something', () => {
+    assert("this is a test", true);
+  });
+});
